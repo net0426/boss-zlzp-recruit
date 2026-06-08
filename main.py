47a4ba841f76cb5330e3207418ad8700a334d3e1
net0426 <919279452>
2026-06-08 22:44:50 +0800

    update: 同步main.py的修改

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from business.recruit_business import RecruitBusiness
 from pages.get_web import DriverManager
+from pages.start_copaw import stop_ai
 import os
+import sys
 import argparse
-
+import atexit
+from pages.email_page import send_email
 
 def parse_args():
     parser = argparse.ArgumentParser(description="招聘岗位智能筛选系统")
@@ -13,6 +16,20 @@ def parse_args():
     return parser.parse_args()
 
 
+def cleanup():
+    """程序退出时的最终清理"""
+    try:
+        stop_ai()
+    except:
+        pass
+    try:
+        DriverManager.close_driver()
+    except:
+        pass
+
+atexit.register(cleanup)
+
+
 if __name__ == '__main__':
     args = parse_args()
 
@@ -21,10 +38,19 @@ if __name__ == '__main__':
     if args.region:
         os.environ['TARGET_REGION'] = args.region
 
-    app = RecruitBusiness(headless=True)
-    if args.platform in ["boss", "all"]:
-        app.boss_ints()
-    if args.platform in ["zlzp", "all"]:
-        app.zlzp_ints()
-
-    DriverManager.close_driver()
+    try:
+        app = RecruitBusiness(headless=True)
+        if args.platform in ["boss", "all"]:
+            if app.boss_ints(): send_email('BOSS运行错误','')
+            else: send_email('BOSS运行成功','')
+        if args.platform in ["zlzp", "all"]:
+            if app.zlzp_ints(): send_email('智联招聘运行错误','')
+            else: send_email('智联招聘运行成功', '')
+    except KeyboardInterrupt:
+        print("\n程序被用户中断")
+    except Exception as e:
+        print(f"程序执行出错: {e}")
+    finally:
+        cleanup()
+        print("程序已退出")
+        sys.exit(0)
